85f4ada2657740daf7678b1a4af82cbe247d4c9c
Patrik Müller <patrik.mueller@ost.ch>
2023-10-02 14:12:00 +0200

    Add packages to setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,10 @@
 
 import setuptools
 
+name = "ipcv_tools"
+packages = setuptools.find_packages(include=name)
 setuptools.setup(
-    name="ipcv_tools",
+    name=name,
     version="0.0.1",
     python_requires=">=3.6",
     install_requires=[
@@ -17,6 +19,7 @@ setuptools.setup(
         "scikit-image",
         "scipy",
     ],
+    packages=packages,
     extras_require={
         "dev": [
             "black",
